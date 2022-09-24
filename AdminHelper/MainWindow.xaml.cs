06b2a9d09a2bd1dea9f61d2a075ee96d3d843886
ChamzyK <chamzykyrgys@mail.ru>
2022-09-24 13:21:21 +0700

    Добавьте файлы проекта.

---
+++ AdminHelper/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace AdminHelper
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
